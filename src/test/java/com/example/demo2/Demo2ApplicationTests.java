692e5c0b1c83047a79281b7a674d4b4ed3f37672
root <1156561743@qq.com>
2018-10-19 17:39:19 +0800

    测试

--- src/test/java/com/example/demo2/Demo2ApplicationTests.java
+++ src/test/java/com/example/demo2/Demo2ApplicationTests.java
@@ -11,7 +11,7 @@ public class Demo2ApplicationTests {
 
     @Test
     public void contextLoads() {
-        System.out.println(123);
+        System.out.println("嘻嘻");
         System.out.println(456);
         System.out.println(788);
         System.out.println(741741741);
