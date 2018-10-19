b58de7abd637cc7940cad0a013158b2eafe6365d
root <1156561743@qq.com>
2018-10-19 17:50:18 +0800

    测试

--- src/test/java/com/example/demo2/Demo2ApplicationTests.java
+++ src/test/java/com/example/demo2/Demo2ApplicationTests.java
@@ -11,7 +11,7 @@ public class Demo2ApplicationTests {
 
     @Test
     public void contextLoads() {
-        System.out.println("阿拉蕾");
+        System.out.println("啧啧啧");
         System.out.println("哈哈");
         System.out.println(456);
         System.out.println(788);
